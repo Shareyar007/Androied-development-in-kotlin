5c538eea874585871b5c05af006a2f3a6d300084
Shareyar Hossain Samir <86526791+Shareyar007@users.noreply.github.com>
2022-07-30 00:01:23 +0600

    Create test.kt

---
+++ test.kt
@@ -0,0 +1,32 @@
+fun main() {
+    println("Hello, world!")
+}
+
+// output : Hello, world!
+
+fun main() {
+    println("Happy Birthday!")
+}
+
+// output : Hello, Birthday!
+
+fun main(){
+    println("Happy birthday")
+	println("Shareyar")
+    println("you are now 20")
+}
+
+/*
+Happy birthday
+Shareyar
+you are now 20
+*/
+
+fun main(){
+    print("Shareyar \n age: 20")
+}
+
+*/
+Shareyar 
+ age: 20
+ */
